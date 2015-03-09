7ed0f9189b6c4074e7907637ce4f363620f21579
Osamu HASEGAWA <good.old.memories.quoll@gmail.com>
2015-03-09 12:13:45 +0900

    classに関連するバグ修正

--- renderer.php
+++ renderer.php
@@ -4,11 +4,13 @@ class block_help_link_renderer extends plugin_renderer_base {
 
 	public function help_link() {
 		$content = html_writer::start_tag('div', array('style'=>'text-align: center;'));
-		$content .= html_writer::start_tag('button', 'btn btn-success viewOptionCheck');
-		$content .= html_writer::start_tag('i', 'icon-white icon-info-sign');
+		$content .= html_writer::start_tag('a', array('target'=>'_blank', 'href'=>'https://moodle-cloud.ealps.shinshu-u.ac.jp/help/'));
+		$content .= html_writer::start_tag('button', array('class'=>'btn btn-success viewOptionCheck'));
+		$content .= html_writer::start_tag('i', array('class'=>'icon-white icon-info-sign'));
 		$content .= html_writer::end_tag('i');
 		$content .= 'eALPSヘルプ';
 		$content .= html_writer::end_tag('button');
+		$content .= html_writer::end_tag('a');
 		$content .= html_writer::end_tag('div');
 	
 /*
