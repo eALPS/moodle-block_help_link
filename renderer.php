26800e0d9e0d55560820d15df8717a5990a4532f
Osamu HASEGAWA <good.old.memories.quoll@gmail.com>
2017-05-23 17:10:20 +0900

    Summary：軽微な変更
    New
    ・なし
    
    Modified
    ・なし
    
    Fixed
    ・ボタンをクリックした際にヘルプページに自動でゲストログインするように変更

--- renderer.php
+++ renderer.php
@@ -3,15 +3,16 @@
 class block_help_link_renderer extends plugin_renderer_base {
 
 	public function help_link() {
-		$content = html_writer::start_tag('div', array('style'=>'text-align: center;'));
-		$content .= html_writer::start_tag('a', array('target'=>'_blank', 'href'=>'https://moodle-cloud.ealps.shinshu-u.ac.jp/help/'));
-		$content .= html_writer::start_tag('button', array('class'=>'btn btn-success viewOptionCheck'));
-		$content .= html_writer::start_tag('i', array('class'=>'icon-white icon-info-sign'));
-		$content .= html_writer::end_tag('i');
+		$content = html_writer::start_tag('form', array('action'=>'https://moodle-cloud.ealps.shinshu-u.ac.jp/help/login/index.php', 'method'=>'post'));
+		$content .= html_writer::start_tag('div', array('style'=>'text-align: center;'));
+		$content .= html_writer::start_tag('button', array('class'=>'btn btn-success'));
+		$content .= html_writer::tag('span', array('class'=>'icon-white icon-info-sign'));
 		$content .= ' eALPSヘルプ';
 		$content .= html_writer::end_tag('button');
-		$content .= html_writer::end_tag('a');
 		$content .= html_writer::end_tag('div');
+		$content .= html_writer::empty_tag('input', array('type'=>'hidden', 'name'=>'username', 'value'=>'guest'));
+		$content .= html_writer::empty_tag('input', array('type'=>'hidden', 'name'=>'password', 'value'=>'guest'));
+		$content .= html_writer::end_tag('form');
 		return $content;
 	}
 
